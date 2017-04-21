4d4785195d8163a29b1af14aee69199d72c68358
wajid-snapad <wajid.khilji@snapad.net>
2017-04-21 17:27:28 +0300

    back to node

---
+++ server.js
@@ -0,0 +1,46 @@
+/**
+ * Created by wajidkhilji on 17/04/2017.
+ */
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+// Get our API routes
+const api = require('./server/routes/api');
+
+const app = express();
+
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use('/src', express.static(path.join(__dirname, 'src')));
+
+// Set our api routes
+app.use('/api', api);
+
+// Catch all other routes and return the index file
+app.get('*', function(req, res)  {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, function() { console.log('API running on localhost:' + port)});
